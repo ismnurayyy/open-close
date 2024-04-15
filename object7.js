d3a0fce2554c79c9ba1c858e3c8747b09595abd0
ismnurayy <ismnnuray1@gmail.com>
2024-04-15 16:33:33 +0400

    open/close

---
+++ object7.js
@@ -0,0 +1,32 @@
+// function head(){
+//     const table = document.getElementById('table');
+//     const header = document.createElement('tr');
+//     const Ad = document.createElement('th');
+//     const Soyad = document.createElement('th');
+//     const Yas = document.createElement('th');
+//     // const  = document.createElement('th');
+
+//     Ad.innerHTML = '<input type="text" placeholder="Ad">';
+//     Soyad.innerHTML = '<input type="text" placeholder="Soyad">';
+//     Yas.innerHTML = '<input type="number" placeholder="Yaş">';
+    
+//     header.appendChild(Ad);
+//     header.appendChild(Soyad);
+//     header.appendChild(yas);
+//     table.appendChild(header);
+// }
+// const button = document.getElementById("button");
+// button.innerHTML = "setir elave et";
+// button.onclick = head;
+
+// const a = document.createElement("td");
+// a.appendChild(button);
+
+// const sonsetir = document.createElement("tr");
+// sonsetir.appendChild(document.createElement("td"));
+// sonsetir.appendChild(document.createElement("td"));
+// sonsetir.appendChild(document.createElement("td"));
+// sonsetir.appendChild(a);
+
+// const table= document.getElementById("table");
+// table.appendChild(sonsetir);
